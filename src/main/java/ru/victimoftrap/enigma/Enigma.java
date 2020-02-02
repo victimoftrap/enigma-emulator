e1b2ad44d83675dda3b45be4b3c5812e9e4d22de
Голованов Александр <monsquizo@gmail.com>
2020-02-02 20:24:46 +0600

    Created Enigma class that contains configuration and signal transformation chain

---
+++ src/main/java/ru/victimoftrap/enigma/Enigma.java
@@ -0,0 +1,61 @@
+package ru.victimoftrap.enigma;
+
+import ru.victimoftrap.enigma.signal.AlphabeticSignal;
+import ru.victimoftrap.enigma.configuration.EnigmaConfiguration;
+import ru.victimoftrap.enigma.chain.SignalTransformationEnigmaChain;
+import ru.victimoftrap.enigma.chain.EnigmaChain;
+
+import java.util.Objects;
+
+public class Enigma {
+    private EnigmaConfiguration configuration;
+    private EnigmaChain enigmaChain;
+
+    public Enigma(final EnigmaConfiguration configuration) {
+        this.configuration = configuration;
+        this.enigmaChain = new SignalTransformationEnigmaChain(
+                configuration.getPlugboard(), configuration.getRotors(), configuration.getReflector()
+        );
+    }
+
+    public String encode(final String source) {
+        final var encodedResult = new StringBuilder();
+
+        for (final var symbol : source.toCharArray()) {
+            char encodedSymbol;
+
+            if (Character.isAlphabetic(symbol)) {
+                var signal = new AlphabeticSignal(symbol);
+                signal = enigmaChain.passTrough(signal);
+                encodedSymbol = signal.getCharacter();
+            } else {
+                encodedSymbol = symbol;
+            }
+
+            encodedResult.append(encodedSymbol);
+        }
+        return encodedResult.toString();
+    }
+
+    public EnigmaConfiguration getConfiguration() {
+        return configuration;
+    }
+
+    public void setConfiguration(final EnigmaConfiguration configuration) {
+        this.configuration = configuration;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Enigma)) return false;
+        Enigma enigma = (Enigma) o;
+        return Objects.equals(configuration, enigma.configuration) &&
+                Objects.equals(enigmaChain, enigma.enigmaChain);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(configuration, enigmaChain);
+    }
+}
